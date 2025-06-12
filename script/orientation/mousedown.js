effe576ccead75503abd3f2b5bf90e00c81607b5
hornokattila <hornokattila@outlook.com>
2025-06-12 10:05:15 +0200

    Changed to only pressed 2.

--- script/orientation/mousedown.js
+++ script/orientation/mousedown.js
@@ -1,17 +1,17 @@
 const image = document.getElementById('main-image');
 const originalSrc = 'img/background.webp';
-let resetTimer = null;
 
-image.addEventListener('click', () => {
+image.addEventListener('mousedown', () => {
     const randomIndex = Math.floor(Math.random() * 10);
     image.src = `img/spray/${randomIndex}.webp`;
+});
 
-    if (resetTimer) {
-        clearTimeout(resetTimer);
-    }
+image.addEventListener('mouseup', () => {
+    image.src = originalSrc;
+});
 
-    resetTimer = setTimeout(() => {
+image.addEventListener('mouseleave', () => {
+    if (image.src !== originalSrc) {
         image.src = originalSrc;
-        resetTimer = null;
-    }, 5000);
+    }
 });
