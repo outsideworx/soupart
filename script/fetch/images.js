b4fdcc4e2425d7dcbd9fb55fc4a03fe250e26f25
hornokattila <hornokattila@outlook.com>
2026-01-18 23:08:02 +0100

    URL change.

--- script/fetch/images.js
+++ script/fetch/images.js
@@ -2,7 +2,7 @@ function loadImages(category) {
     const urlParams = new URLSearchParams(window.location.search);
     const offset = urlParams.get("offset");
     $.ajax({
-        url: `/api/soupart?category=${category}&offset=${offset}`,
+        url: `/api/cached/soupart?category=${category}&offset=${offset}`,
         method: 'GET',
         success: function (response) {
             if (response) {
